afbcdab41500d2dedeed132c60e1700710841440
stackTom <stackTom@users.noreply.github.com>
2023-03-05 12:47:44 -0500

    read viewport from osm file

--- AutomaticWaterMaskingTest/Program.cs
+++ AutomaticWaterMaskingTest/Program.cs
@@ -13,15 +13,9 @@ namespace AutomaticWaterMaskingTest
             string outPath = Environment.GetCommandLineArgs()[1];
             string coastXML = File.ReadAllText(outPath + @"\coast.osm");
             string waterXML = File.ReadAllText(outPath + @"\water.osm");
-            DownloadArea d = new DownloadArea(-80.1238138888889m, -80.1195222222222m, 26.1076277777778m, 26.1075555555556m);
-            Way<AutomaticWaterMasking.Point> viewPort = new Way<AutomaticWaterMasking.Point>();
-            viewPort.Add(new AutomaticWaterMasking.Point(d.startLon, d.startLat));
-            viewPort.Add(new AutomaticWaterMasking.Point(d.endLon, d.startLat));
-            viewPort.Add(new AutomaticWaterMasking.Point(d.endLon, d.endLat));
-            viewPort.Add(new AutomaticWaterMasking.Point(d.startLon, d.endLat));
-            viewPort.Add(new AutomaticWaterMasking.Point(d.startLon, d.startLat));
-            string xml = viewPort.ToOSMXML();
-            File.WriteAllText(@"C:\users\fery2\Desktop\VIEWPORT.osm", xml);
+            string viewPortXML = File.ReadAllText(outPath + @"\viewport.osm");
+            Way<AutomaticWaterMasking.Point> viewPort = new List<Way<AutomaticWaterMasking.Point>>(AreaKMLFromOSMDataCreator.GetWays(viewPortXML, true).Values)[0];
+
             List<Way<AutomaticWaterMasking.Point>> polygons = WaterMasking.CreatePolygons(coastXML, waterXML, viewPort);
             return;
             Bitmap bmp = WaterMasking.GetMask(outPath, 4096, 4096, new AutomaticWaterMasking.Point(26.1075555555556m, -80.1195222222222m), new AutomaticWaterMasking.Point(26.1076277777778m, -80.1238138888889m), polygons);
