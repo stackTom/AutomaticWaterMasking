62f7ab3dee7ffd1bcf8068f92d031573d9c07f0d
stackTom <stackTom@users.noreply.github.com>
2023-03-30 11:31:31 -0400

    update AutomaticWaterMaskingTest
    creates coast water polygons from given start latitude and longitudes
    
    TODO: specify end lat and long and also create the non coast polygons

---
+++ AutomaticWaterMaskingTest/AutomaticWaterMaskingTest.cs
@@ -0,0 +1,47 @@
+﻿using AutomaticWaterMasking;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class AutomaticWaterMaskingTest
+{
+    public static void Main(string[] args)
+    {
+        string[] cmdArgs = Environment.GetCommandLineArgs();
+        string outPath = cmdArgs[3];
+        int startAt = Int32.Parse(cmdArgs[1]);
+        for (int startLat = Int32.Parse(cmdArgs[2]); startLat < 70; startLat++)
+        {
+            for (int startLon = startAt; startLon < 180; startLon++)
+            {
+                List<Way<AutomaticWaterMasking.Point>> coastWaterPolygons = new List<Way<AutomaticWaterMasking.Point>>();
+                List<Way<AutomaticWaterMasking.Point>>[] inlandPolygons = new[] {
+                    new List<Way<AutomaticWaterMasking.Point>>(), new List<Way<AutomaticWaterMasking.Point>>(), new List<Way<AutomaticWaterMasking.Point>>()
+                };
+
+                Console.WriteLine("Creating " + startLat.ToString() + ", " + startLon.ToString());
+                DownloadArea d = new DownloadArea(startLon, startLon + 1, startLat + 1, startLat);
+                Way<AutomaticWaterMasking.Point> viewPort = new Way<AutomaticWaterMasking.Point>();
+                viewPort.Add(new AutomaticWaterMasking.Point((decimal)startLon, (decimal)(startLat + 1)));
+                viewPort.Add(new AutomaticWaterMasking.Point((decimal)startLon + 1, (decimal)(startLat + 1)));
+                viewPort.Add(new AutomaticWaterMasking.Point((decimal)(startLon + 1), (decimal)startLat));
+                viewPort.Add(new AutomaticWaterMasking.Point((decimal)startLon, (decimal)startLat));
+                viewPort.Add(new AutomaticWaterMasking.Point((decimal)startLon, (decimal)(startLat + 1)));
+
+                string coastXML = WaterMasking.DownloadOsmCoastData(d, outPath + Path.DirectorySeparatorChar + "coast.osm");
+                try
+                {
+                    WaterMasking.CreatePolygons(coastWaterPolygons, inlandPolygons, coastXML, null, viewPort);
+                    string coastWater = OSMXMLParser.WaysToOSMXML(coastWaterPolygons);
+                    File.WriteAllText(outPath + Path.DirectorySeparatorChar + "coastWaterPolys.osm", coastWater);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("failed " + startLat.ToString() + ", " + startLon.ToString());
+                    Console.WriteLine(e.ToString());
+                    return;
+                }
+            }
+        }
+    }
+}
